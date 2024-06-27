dc869a780a6b2b3e6f252c1e32c2febb0bb0e656
Juks0 <129502580+Juks0@users.noreply.github.com>
2024-06-27 20:57:04 +0200

    added gitignore

--- obj/Debug/net8.0/APBD3.AssemblyInfo.cs
+++ obj/Debug/net8.0/APBD3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APBD3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bdb3dc2efdf7c9171ab70ce659db41f86a9525c")]
 [assembly: System.Reflection.AssemblyProductAttribute("APBD3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APBD3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
